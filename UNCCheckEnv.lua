f0a549eb129632aabe31c5c54a886a65632b1319
Sero (@serotuko) <159341819+theonlylegacy@users.noreply.github.com>
2025-05-31 19:43:31 +0200

    Update UNCCheckEnv.lua

--- UNCCheckEnv.lua
+++ UNCCheckEnv.lua
@@ -163,11 +163,11 @@ end)
 test("getcallingscript", {})
 
 test("getscriptclosure", {"getscriptfunction"}, function()
-	local module = game:GetService("CoreGui").RobloxGui.Modules.Common.Constants
-	local constants = getrenv().require(module)
+	local module = game:GetService("CoreGui").RobloxGui.Modules.Common.CommonUtil
+	local commonuntil = getrenv().require(module)
 	local generated = getscriptclosure(module)()
-	assert(constants ~= generated, "Generated module should not match the original")
-	assert(shallowEqual(constants, generated), "Generated constant table should be shallow equal to the original")
+	assert(commonuntil ~= generated, "Generated module should not match the original")
+	assert(shallowEqual(commonuntil, generated), "Generated constant table should be shallow equal to the original")
 end)
 
 test("hookfunction", {"replaceclosure"}, function()
